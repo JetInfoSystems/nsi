7db0653050f7983304fd6807a7d10ff303ec39b3
Ruslan Markelov <markelov@jet.msk.su>
2015-06-01 14:06:59 +0300

    RNSC-444 заглушки для сервисов чтобы можно было собрать бандл

---
+++ services/src/main/java/jet/isur/nsi/services/impl/NsiMetaServiceImpl.java
@@ -0,0 +1,21 @@
+package jet.isur.nsi.services.impl;
+
+import jet.isur.nsi.api.NsiMetaService;
+import jet.isur.nsi.api.model.MetaDictGetReq;
+import jet.isur.nsi.api.model.MetaDictGetRes;
+import jet.isur.nsi.api.model.MetaDictListReq;
+import jet.isur.nsi.api.model.MetaDictListRes;
+
+public class NsiMetaServiceImpl implements NsiMetaService {
+
+    @Override
+    public MetaDictListRes metaDictList(MetaDictListReq req) {
+        return null;
+    }
+
+    @Override
+    public MetaDictGetRes metaDictGet(MetaDictGetReq req) {
+        return null;
+    }
+
+}
---
+++ services/src/main/java/jet/isur/nsi/services/impl/NsiServiceImpl.java
@@ -0,0 +1,42 @@
+package jet.isur.nsi.services.impl;
+
+import jet.isur.nsi.api.NsiService;
+import jet.isur.nsi.api.model.DictCountReq;
+import jet.isur.nsi.api.model.DictCountRes;
+import jet.isur.nsi.api.model.DictDeleteReq;
+import jet.isur.nsi.api.model.DictDeleteRes;
+import jet.isur.nsi.api.model.DictGetReq;
+import jet.isur.nsi.api.model.DictGetRes;
+import jet.isur.nsi.api.model.DictListReq;
+import jet.isur.nsi.api.model.DictListRes;
+import jet.isur.nsi.api.model.DictSaveReq;
+import jet.isur.nsi.api.model.DictSaveRes;
+
+public class NsiServiceImpl implements NsiService {
+
+    @Override
+    public DictCountRes dictCount(DictCountReq req) {
+        return null;
+    }
+
+    @Override
+    public DictListRes dictList(DictListReq req) {
+        return null;
+    }
+
+    @Override
+    public DictGetRes dictGet(DictGetReq req) {
+        return null;
+    }
+
+    @Override
+    public DictSaveRes dictSave(DictSaveReq req) {
+        return null;
+    }
+
+    @Override
+    public DictDeleteRes dictDelete(DictDeleteReq req) {
+        return null;
+    }
+
+}
