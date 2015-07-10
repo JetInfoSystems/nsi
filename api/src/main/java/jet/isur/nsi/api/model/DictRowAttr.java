d6d367743864b0702d598ac4b22d271e175c83bf
Pavel Mironenko <pmironenko@jet.msk.su>
2015-07-10 18:02:43 +0300

    RNSC-448 добавлен утилитный метод получения первого value у DictRowAttr

--- api/src/main/java/jet/isur/nsi/api/model/DictRowAttr.java
+++ api/src/main/java/jet/isur/nsi/api/model/DictRowAttr.java
@@ -35,6 +35,13 @@ public class DictRowAttr implements Serializable {
         values.add(value);
     }
 
+    public String getFirstValue() {
+        if(values != null && !values.isEmpty()) {
+            return values.iterator().next();
+        }
+        return null;
+    }
+
     public Map<String, DictRowAttr> getRefAttrs() {
         return refAttrs;
     }
