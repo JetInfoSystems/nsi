b69974a27d4697ce8b1bcf37f8efbcdee74768fc
cutgun <cutgun@gmail.com>
2016-10-23 23:06:37 +0300

    В рамках AFS-63 созданы методы для создания и изменения метаданных

---
+++ common/src/main/java/jet/nsi/common/helper/MetaDictGen.java
@@ -0,0 +1,110 @@
+package jet.nsi.common.helper;
+
+import jet.nsi.api.model.MetaAttrType;
+import jet.nsi.api.model.MetaFieldType;
+import jet.nsi.api.model.builder.MetaDictBuilder;
+import jet.nsi.api.model.builder.MetaDictBuilderImpl;
+
+/**
+ * Created by akatkevich on 23.10.2016.
+ */
+public class MetaDictGen {
+
+    public static MetaDictBuilder genMetaDict(String name) {
+        return new MetaDictBuilderImpl()
+                .name(name)
+                .caption("")
+                .table(null)
+                //Fields
+                .field()
+                    .name("id")
+                    .size(19)
+                    .type(MetaFieldType.NUMBER)
+                    .add()
+                .field()
+                    .name("is_deleted")
+                    .size(1)
+                    .type(MetaFieldType.BOOLEAN)
+                    .add()
+                .field()
+                    .name("last_change")
+                    .type(MetaFieldType.DATE_TIME)
+                    .add()
+                .field()
+                    .name("last_user")
+                    .size(19)
+                    .type(MetaFieldType.NUMBER)
+                    .add()
+               /* .field()
+                    .name("f1")
+                    .size(100)
+                    .type(MetaFieldType.VARCHAR)
+                    .add()
+                .field()
+                    .name("ORG_ID")
+                    .size(19)
+                    .type(MetaFieldType.NUMBER)
+                    .add()
+                .field()
+                    .name("ORG_ROLE_ID")
+                    .size(19)
+                    .type(MetaFieldType.NUMBER)
+                    .add()
+                //Attrs
+                .attr()
+                    .addField("f1")
+                    .type(MetaAttrType.VALUE)
+                    .caption("f 1")
+                    .hidden(false)
+                    .name("f1")
+                    .add() */
+                .attr()
+                    .addField("id")
+                    .type(MetaAttrType.VALUE)
+                    .caption("id")
+                    .hidden(false)
+                    .createOnly(true)
+                    .name("id")
+                    .required(true)
+                    .add()
+                .attr()
+                    .addField("is_deleted")
+                    .type(MetaAttrType.VALUE)
+                    .caption("is_deleted")
+                    .hidden(false)
+                    .name("is_deleted")
+                    .add()
+                .attr()
+                    .addField("last_change")
+                    .type(MetaAttrType.VALUE)
+                    .caption("last change")
+                    .hidden(true)
+                    .name("last_change")
+                    .add()
+                .attr()
+                    .addField("last_user")
+                    .type(MetaAttrType.VALUE)
+                    .caption("last_user")
+                    .hidden(false)
+                    .name("last_user")
+                    .add()
+                /*.attr().addField("ORG_ID").type(MetaAttrType.VALUE)
+                .caption("org").name("ORG_ID").add().attr()
+                .addField("ORG_ROLE_ID").type(MetaAttrType.VALUE)
+                .caption("org_role").name("ORG_ROLE_ID")
+                .add()*/
+                //Others
+                .idAttr("id")
+                .deleteMarkAttr("is_deleted")
+                .lastChangeAttr("last_change")
+                .lastUserAttr("last_user")
+                //.addCaptionAttr("f1")
+                //.addRefObjectAttr("f1")
+                //.addLoadDataAttr("id")
+                //.addTableObjectAttr("f1")
+                //.addOwn("ORG", "ORG_ID")
+                //.addOwn("ORG_ROLE", "ORG_ROLE_ID")
+                //.addInterceptor("interceptor")
+                ;
+    }
+}
