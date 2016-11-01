0654237f0173d37e66c9f33c3fa3727c28114be1
Ann Sukhanova <sukhanova@jet.msk.su>
2016-11-01 11:27:43 +0300

    AFS-80 fix limit queries

--- common/src/main/java/jet/nsi/common/platform/postgresql/PostgresqlPlatformSqlGen.java
+++ common/src/main/java/jet/nsi/common/platform/postgresql/PostgresqlPlatformSqlGen.java
@@ -1,17 +1,9 @@
 package jet.nsi.common.platform.postgresql;
 
 import static org.jooq.impl.DSL.field;
-import static org.jooq.impl.DSL.val;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import org.jooq.Condition;
 import org.jooq.Field;
-import org.jooq.Query;
-import org.jooq.Record;
-import org.jooq.SelectConditionStep;
-import org.jooq.SelectField;
 import org.jooq.SelectJoinStep;
 
 import jet.nsi.api.data.NsiConfigField;
@@ -37,31 +29,6 @@ public class PostgresqlPlatformSqlGen extends DefaultPlatformSqlGen {
             return new PostgresqlTextSearchCondition(f);
         default:
             return super.getFieldFuncCondition(query, field, filter, baseQuery);
-        }
-        
-    }
-    
-    @Override
-    public Query limit(SelectJoinStep<?> baseQuery, long offset, int size) {
-        if (size != -1) {
-            List<SelectField<?>> limitFields = new ArrayList<>();
-            limitFields.add(field("a.*"));
-            limitFields.add(field("ROWNUM").as("rnum"));
-            
-            SelectConditionStep<Record> limitQuery = getQueryBuilder()
-                    .select(limitFields)
-                    .from(baseQuery.asTable("a"))
-                    .where(field("ROWNUM").lessThan(val((Object)null)));
-            if(offset != -1) {
-                return getQueryBuilder()
-                    .select(field("b.*"))
-                    .from(limitQuery.asTable("b"))
-                    .where(field("rnum").greaterOrEqual(val((Object)null)));
-            } else {
-                return limitQuery;
-            }
-        } else {
-            return baseQuery;
         }
     }
 }
